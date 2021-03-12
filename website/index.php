f0bdc06434568a7817ea17d97596dfa650ab29cd
Christian Schnittert <christian2424@outlook.de>
2021-03-12 15:12:06 +0100

    Website - Formatierung

--- website/index.php
+++ website/index.php
@@ -2,12 +2,11 @@
 <head>
 
 </head>
-<body>
-<form action="" method="Post">
-<label for="stock">Stock:</label><br>
-<input type="text" name="stock" value="" ><br><br>
-<input type="submit" name="submit" value="Submit">
-</form>
-
-</body>
+    <body>
+        <form action="" method="Post">
+            <label for="stock">Stock:</label><br>
+            <input type="text" name="stock" value="" ><br><br>
+            <input type="submit" name="submit" value="Submit">
+        </form>
+    </body>
 </html>
