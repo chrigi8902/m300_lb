2ac83aa71e3455ff459ed13b179abe41c7ed1e08
Christian Schnittert <christian2424@outlook.de>
2021-03-12 13:58:41 +0100

    Website - Form

---
+++ website/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html><html  >
+<head>
+
+</head>
+<body>
+<form action="" method="Post">
+<label for="stock">Stock:</label><br>
+<input type="text" name="stock" value="" ><br><br>
+<input type="submit" name="submit" value="Submit">
+</form>
+
+</body>
+</html>
