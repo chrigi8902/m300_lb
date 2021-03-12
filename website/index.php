a34f716a42453032361aa2147ed1a82f23ec4755
Christian Schnittert <christian2424@outlook.de>
2021-03-12 16:17:03 +0100

    Website funktioniert mit python

--- website/index.php
+++ website/index.php
@@ -3,19 +3,18 @@
 
 </head>
     <body>
+        <?php
+            $valor = $_POST['stock'];
+            $price = exec("python ./python/get_price.py $valor 2<&1");
+            $valor_py = file_get_contents( "./python/tmp/stock.txt" );
+            $price_py = file_get_contents( "./python/tmp/price.txt" );
+            echo "Price for: " .$valor_py ."is: " .$price_py ;
+        ?>
         <form action="" method="Post">
             <label for="stock">Stock:</label><br>
             <input type="text" name="stock" value="" ><br><br>
             <input type="submit" name="submit" value="Submit">
         </form>
 
-        <?php 
-            function pre_r( $array )
-            {
-                $valor = $_POST['stock'];
-                $price = exec("python ./python/get_price.py $valor 2<&1");
-                sleep (5);
-            }
-        ?>
     </body>
 </html>
