522bcd1f4f56cf9bd379c0ea06b015735dd09972
Christian Schnittert <christian2424@outlook.de>
2021-03-13 22:54:18 +0100

    Webseiten anpassungen

--- website/index.php
+++ website/index.php
@@ -1,20 +1,24 @@
 <!DOCTYPE html><html  >
 <head>
-
+    <link rel="stylesheet" href="./css/style.css">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 </head>
     <body>
-        <?php
-            $valor = $_POST['stock'];
-            $price = exec("python3.6 ./python/get_price.py $valor 2<&1");
-            $valor_py = file_get_contents( "./python/tmp/stock" );
-            $price_py = file_get_contents( "./python/tmp/price" );
-            echo "Price for: " .$valor_py ."is: " .$price_py ;
-        ?>
-        <form action="" method="Post">
-            <label for="stock">Stock:</label><br>
-            <input type="text" name="stock" value="" ><br><br>
-            <input type="submit" name="submit" value="Submit">
-        </form>
-
+        <div>
+            <h1>
+            <?php
+                $valor = $_POST['stock'];
+                $price = exec("python3.6 ./python/get_price.py $valor 2<&1");
+                $valor_py = file_get_contents( "./python/tmp/stock" );
+                $price_py = file_get_contents( "./python/tmp/price" );
+                echo "Price for: " .$valor_py ."is: " .$price_py ;
+            ?>
+            <form action="" method="Post">
+                <label for="stock">Stock:</label><br>
+                <input type="text" id="stockin" name="stock" value="" ><br>
+                <input type="submit" id="stocksub" name="submit" value="Submit">
+            </form>
+            </h1>
+        </div>
     </body>
 </html>
