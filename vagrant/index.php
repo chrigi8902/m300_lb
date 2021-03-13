54650e552b589e9a48e05a0bce99c54b3b3185f4
Christian Schnittert <christian2424@outlook.de>
2021-03-13 23:08:10 +0100

    Webseiten anpassungen

--- vagrant/index.php
+++ vagrant/index.php
@@ -5,20 +5,25 @@
 </head>
     <body>
         <div>
+            
+
             <h1>
-            <?php
-                $valor = $_POST['stock'];
-                $price = exec("python3.6 ./python/get_price.py $valor 2<&1");
-                $valor_py = file_get_contents( "./python/tmp/stock" );
-                $price_py = file_get_contents( "./python/tmp/price" );
-                echo "Price for: " .$valor_py ."is: " .$price_py ;
-            ?>
             <form action="" method="Post">
                 <label for="stock">Stock:</label><br>
                 <input type="text" id="stockin" name="stock" value="" ><br>
                 <input type="submit" id="stocksub" name="submit" value="Submit">
             </form>
             </h1>
+            <h2>
+            <?php
+                $valor = $_POST['stock'];
+                $price = exec("python3.6 ./python/get_price.py $valor 2<&1");
+                $valor_py = file_get_contents( "./python/tmp/stock" );
+                $price_py = file_get_contents( "./python/tmp/price" );
+                echo "Price for: " .$valor_py ."is: ";
+                echo $price_py ;
+            ?>
+            </h2>
         </div>
     </body>
 </html>
