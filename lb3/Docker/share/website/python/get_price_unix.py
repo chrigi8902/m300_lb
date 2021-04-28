c1d42a9d3440695904d20c93c47455d87ef6c1fd
Christian Schnittert <christian2424@outlook.de>
2021-04-28 20:43:40 +0200

    python funktioniert auf unix

--- lb3/Docker/share/website/python/get_price_unix.py
+++ lb3/Docker/share/website/python/get_price_unix.py
@@ -10,18 +10,24 @@ class Event(LoggingEventHandler):
         from yahoo_fin import stock_info as si
 
         #stock = str(sys.argv[1])
-        stock = "amc"
-
-        price = si.get_live_price(stock)
-        price_str = str(price)
         file_price = "/usr/src/app/tmp/price.txt"
         file_stock = "/usr/src/app/tmp/stock.txt"
+        
+        #price_str = str(price)
+        if (event.src_path != "./tmp/price.txt") and (event.src_path != "./tmp/stock.txt") and (event.src_path != "./tmp"):
+                stock_path = event.src_path
+                stock = stock_path.replace("./tmp/", "")
+                price = si.get_live_price(stock)
+                price_str = str(price)
 
-        with open(file_price, 'w') as fileowrite:
-                fileowrite.write(price_str+"<br>")
-
-        with open(file_stock, 'w') as fileowrite:
-                fileowrite.write(stock+"<br>")
+                print (price_str)
+                print (stock)
+                with open(file_price, 'w') as fileowrite:
+                        fileowrite.write(price_str)
+                with open(file_stock, 'w') as fileowrite:
+                        fileowrite.write(stock)
+        else:
+                print("\\nothing")
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
