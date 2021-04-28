014b497ab89ef8f8641cb8a4b73b573a134e1c02
Christian Schnittert <christian2424@outlook.de>
2021-04-28 20:25:52 +0200

    python funktioniert auf win

--- lb3/Docker/share/website/python/get_price_unix.py
+++ lb3/Docker/share/website/python/get_price_unix.py
@@ -1,15 +1,40 @@
 import sys
-from yahoo_fin import stock_info as si
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
 
-stock = str(sys.argv[1])
+class Event(LoggingEventHandler):
+    def dispatch(self, event):
+        import sys
+        from yahoo_fin import stock_info as si
 
-price = si.get_live_price(stock)
-price_str = str(price)
-file_price = "/var/www/html/python/tmp/price"
-file_stock = "/var/www/html/python/tmp/stock"
+        #stock = str(sys.argv[1])
+        stock = "amc"
 
-with open(file_price, 'w') as fileowrite:
-        fileowrite.write(price_str+"<br>")
+        price = si.get_live_price(stock)
+        price_str = str(price)
+        file_price = "/usr/src/app/tmp/price.txt"
+        file_stock = "/usr/src/app/tmp/stock.txt"
 
-with open(file_stock, 'w') as fileowrite:
-        fileowrite.write(stock+"<br>")
+        with open(file_price, 'w') as fileowrite:
+                fileowrite.write(price_str+"<br>")
+
+        with open(file_stock, 'w') as fileowrite:
+                fileowrite.write(stock+"<br>")
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    event_handler = Event()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
